202de3a5146a2eb97e8fdaf0b195c6ab4eb5d303
anas bouhrim <anasbhr1@hotmail.com>
2025-02-22 23:04:16 +0100

    Update: Add library_detail.html template in relationship_app and update LibraryDetailView to import Library and use context 'library'

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import LibraryDetailView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
+    # URL pattern for the function-based view that lists all books
+    path('books/', list_books, name='list_books'),
+
+    # URL pattern for the class-based view that displays library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
