961273b3e070d0856e1af2fd0d17d9eccb825a16
anas bouhrim <anasbhr1@hotmail.com>
2025-02-23 02:19:43 +0100

    Updated urls: added paths for add_book and edit_book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
     path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
+    path('book/add/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
