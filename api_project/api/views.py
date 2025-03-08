81663ae24d592ef190c2294ad3ba4151eaa73bdf
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-08 18:02:56 +0000

    Add token authentication and permissions for API access

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,30 +1,13 @@
-#from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-# Create your views here.
-class BookList(ListAPIView):
-    queryset = Book.objects.all() #to retrieve all books from the db
-    serializer_class = BookSerializer # use bookserializer to format data
+from rest_framework import viewsets
 
-class BookViewSet(viewsets.ModelViewSet): # view set to CRUD operations
-    queryset = Book.objects.all() #to retrieve all books
-    serializer_class = BookSerializer #to format data using book serializer
+class BookList(generics.ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()  # This will retrieve all books from the database
 
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.contrib.auth.models import User
-from rest_framework.permissions import IsAuthenticated
-
-
-
-class CustomAuthToken(ObtainAuthToken):
-     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'user_id': user.id, 'username': user.username})
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
