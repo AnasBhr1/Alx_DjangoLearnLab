06d8d50559d41e1a3f230c34183dfe8f00cfef14
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-08 18:00:40 +0000

    Add token authentication and permissions for API access

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet 
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 
 # Initialize the router
 router = DefaultRouter()
@@ -11,4 +13,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Include the router URLs for CRUD operations on the Book model
     path('', include(router.urls)),  # Automatically generates CRUD routes
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,5 +7,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # This will retrieve all books from the database
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()  # Get all books
-    serializer_class = BookSerializer  # Use the BookSerializer
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
