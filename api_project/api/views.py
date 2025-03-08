e3925d55d1a4d1bc44a6b841ae75f665a5b5fac1
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-08 13:21:14 +0000

    Add Book API endpoint with DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,7 @@
-#from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-# Create your views here.
+
 class BookList(ListAPIView):
-    queryset = Book.objects.all() #to retrieve all books from the db
-    serializer_class = BookSerializer # use bookserializer to format data
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
