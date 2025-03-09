05cacfb858a4b32511124ea243de32e30f475396
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-09 13:07:57 +0000

    Add token authentication and permissions for API access

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,6 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-from .models import YourModel
-from .serializers import YourModelSerializer
 
 
 class BookList(generics.ListAPIView):
