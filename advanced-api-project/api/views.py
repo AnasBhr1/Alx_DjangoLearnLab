29140e9efc99b2ddae561abc1ecbf8f1b16a8d1a
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-13 23:20:06 +0000

    Configured URL routing for book views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListCreateAPIView):
+    """Handles listing all books and creating a new book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Handles retrieving, updating, and deleting a single book by ID."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
