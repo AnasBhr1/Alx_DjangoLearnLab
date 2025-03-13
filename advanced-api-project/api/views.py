f2d1e3fb96aa98345b95cba3d0f23daebb03ae40
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-13 23:24:01 +0000

    Implemented generic views for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from api.models import Book
+from api.serializers import BookSerializer
 
 class BookListView(generics.ListCreateAPIView):
-    """Handles listing all books and creating a new book."""
+    """Handles listing all books and creating a new book"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Handles retrieving, updating, and deleting a single book by ID."""
+    """Handles retrieving, updating, and deleting a single book"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
