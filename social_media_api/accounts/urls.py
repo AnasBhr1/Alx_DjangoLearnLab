fb26244b650f631f49567ada9c413f1ba8156db7
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-29 13:38:17 +0000

    Added follow/unfollow and user feed features

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,11 @@
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework import filters
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
